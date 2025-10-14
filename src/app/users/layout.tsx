ce4d03468fe12331203a15937ab229f44f2a19e2
EmmaMaltseva <emmamaltseva0@gmail.com>
2025-10-14 22:18:27 +0300

    добавила код на страницу create/page.tsx: импортировала форму и элементы формы из antd, импортировала addUser из zustand хранилища, создала yup объект и задала правила валидных полей, с помощью хука useForm получила функции и данные для обработки полей и вывода ошибок, добавила дефолтное значение в селектор, добавила комопнент Controller для корректной валидации полей, добавила функцию onSubmit для добавления нового пользователя в хранилище и переход на страницу с таблицей, добавила кнопку отменить для возврата на страницу пользователей и прекратить создание пользователя

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -3,7 +3,7 @@ export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-16 py-12 min-h-screen flex flex-col">
+    <main className="ontainer mx-auto px-4 sm:px-6 md:px-8 lg:px-16 py-12 min-h-screen flex flex-col items-center">
       {children}
     </main>
   );
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,8 +2,8 @@ import UserTable from "@/components/UserTable";
 
 export default async function Page() {
   return(
-    <div> 
-      <h1 className="text-xl text-gray-800 md:text-2xl md:leading-normal ">Таблица пользователей</h1>
+    <div className="w-full"> 
+      <h1 className="text-xl text-gray-800 md:text-2xl md:leading-normal">Таблица пользователей</h1>
       <UserTable />
     </div>
   );
