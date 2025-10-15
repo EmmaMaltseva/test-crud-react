216f86e1592febde51cf5a1c3d7ce9ada78d8d65
EmmaMaltseva <emmamaltseva0@gmail.com>
2025-10-16 00:11:34 +0300

    заменила vh на dvh для корректной высоты в ios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "antd";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center gap-5 min-h-screen container mx-auto px-4 sm:px-6 md:px-8 lg:px-16 py-8 md:py-12">
+    <main className="flex flex-col items-center justify-center gap-5 min-h-[100dvh] container mx-auto px-4 sm:px-6 md:px-8 lg:px-16 py-8 md:py-12">
       <h1 className="text-xl text-gray-800 md:text-3xl md:leading-normal text-center dark:text-white">Тестовое задание: CRUD приложение на React</h1>
       <Link href="/users" passHref>
         <Button type="primary" size="large">Вперёд!</Button>
